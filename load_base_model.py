03db3bd87d0b1b2449871f889fc99875ef978bde
Jackson Shen <shenj7@rose-hulman.edu>
2026-03-31 16:07:20 -0400

    check if base llada works

---
+++ load_base_model.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="GSAI-ML/LLaDA-8B-Instruct", trust_remote_code=True)
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe(messages)
