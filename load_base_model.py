d39183decfea74dfc50000b3254a0630017480e1
Jackson Shen <shenj7@rose-hulman.edu>
2026-03-31 16:28:03 -0400

    updog

--- load_base_model.py
+++ load_base_model.py
@@ -3,5 +3,6 @@ from transformers import pipeline
 pipe = pipeline("text-generation", model="GSAI-ML/LLaDA-8B-Instruct", trust_remote_code=True)
 messages = [
     {"role": "user", "content": "Who are you?"},
+    {"role": "user", "content": "Does it smell like updog in here?"},
 ]
 pipe(messages)
